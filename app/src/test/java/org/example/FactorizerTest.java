3e2cb863a07184302616a25a6c6405d20602d2ab
jsb999 <90780236+jsb999@users.noreply.github.com>
2025-09-09 21:14:27 +0000

    Added Tests

--- app/src/test/java/org/example/FactorizerTest.java
+++ app/src/test/java/org/example/FactorizerTest.java
@@ -1,13 +1,44 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
 class FactorizerTest {
+  @BeforeEach
+  void setUp(){
+    Factorizer factorizer = new Factorizer();
+  }
+  
   @Test
   void itReturnsAnEmptyListForZero() {
-    Factorizer factorizer = new Factorizer();
-    assertEquals(factorizer.primeFactors(0), new ArrayList<Integer>());
+assertEquals(factorizer.primeFactors(0), new ArrayList<Integer>());
+  }
+
+  @Test
+  void itReturnsAnEmptyListForOne(){
+    assertEquals(factorizer.primeFactors(1), new ArrayList<Integer>());
+  }
+
+  @Test
+  void itReturnsListForFifty(){
+    ArrayList<Integer> expected = new ArrayList<Integer>();
+    expected.add(2);
+    expected.add(5);
+    expected.add(10);
+    expected.add(25);
+
+    assertEquals(factorizer.primeFactors(50), expected);
+  }
+
+  @Test
+  void itReturnsTrueForPrime(){
+    assertTrue(factorizer.isPrime(43));
+  }
+
+  @Test
+  void itReturnsFalseForComposite(){
+    assertFalse(factorizer.isComposite(43));
   }
 }
