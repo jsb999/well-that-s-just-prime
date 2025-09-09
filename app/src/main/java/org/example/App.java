42385ab592e947aeeec23733f72b2166e2dfa2b7
jsb999 <90780236+jsb999@users.noreply.github.com>
2025-09-09 20:58:41 +0000

    finished code

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -20,12 +20,20 @@ public class App {
     System.out.println("10: " + prime.isPrime(10));
     System.out.println("100: " + prime.isPrime(100));
 
-    System.out.println("\nAre these primes?");
+    System.out.println("\nAre these composite?");
     System.out.println("0: " + prime.isComposite(0));
     System.out.println("1: " + prime.isComposite(1));
     System.out.println("2: " + prime.isComposite(2));
     System.out.println("43: " + prime.isComposite(43));
     System.out.println("10: " + prime.isComposite(10));
     System.out.println("100: " + prime.isComposite(100));
+
+    System.out.println("\nReduce these fractions:");
+    System.out.println("1/1: " + prime.reduce(1, 1));
+    System.out.println("1/2: " + prime.reduce(1, 2));
+    System.out.println("1/3: " + prime.reduce(1, 3));
+    System.out.println("2/3: " + prime.reduce(2, 3));
+    System.out.println("2/4: " + prime.reduce(2, 4));
+    
   }
 }
--- app/src/main/java/org/example/Factorizer.java
+++ app/src/main/java/org/example/Factorizer.java
@@ -4,29 +4,29 @@ import java.util.ArrayList;
 
 public class Factorizer {
   public ArrayList<Integer> primeFactors(int num){
-      ArrayList<Integer> primeReturns = new ArrayList<Integer>();
+    ArrayList<Integer> primeReturns = new ArrayList<Integer>();
 
-      int counter = 1;
-    
-      while(counter < num/2){
-        counter++;
-        if(num % counter == 0){
-          primeReturns.add(counter);
-        }
-      }
-
-      return primeReturns;
-    }
-
-    public boolean isPrime(int num){
-      if(primeFactors(num).size() == 0){
-        return true;
-      } else {
-        return false;
+    int counter = 1;
+  
+    while(counter < num/2){
+      counter++;
+      if(num % counter == 0){
+        primeReturns.add(counter);
       }
     }
 
-    public boolean isComposite(int num){
-      return !isPrime(num);
+    return primeReturns;
+  }
+
+  public boolean isPrime(int num){
+    if(primeFactors(num).size() == 0){
+      return true;
+    } else {
+      return false;
     }
   }
+
+  public boolean isComposite(int num){
+    return !isPrime(num);
+  }
+}
